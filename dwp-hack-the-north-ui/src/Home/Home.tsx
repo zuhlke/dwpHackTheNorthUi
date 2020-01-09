6c8953086da9b3915420a9a18eeab476e6337174
Andrew Stone <Andrew.Stone@zuhlke.com>
2020-01-09 16:32:22 +0000

    Link from home screen to questionnaire.

--- dwp-hack-the-north-ui/src/Home/Home.tsx
+++ dwp-hack-the-north-ui/src/Home/Home.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 export const Home: React.FC = () => {
     return (
         <div>
             Home screen
+            <Link to={"/Questionnaire"}>Questionnaire</Link>
         </div>
     )
 };
