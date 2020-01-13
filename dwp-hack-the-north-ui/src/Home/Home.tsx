fd015ec035f140ea292d706c4bfd42ae0a6157d9
Andrew Stone <Andrew.Stone@zuhlke.com>
2020-01-13 15:48:41 +0000

    Text for home page.

--- dwp-hack-the-north-ui/src/Home/Home.tsx
+++ dwp-hack-the-north-ui/src/Home/Home.tsx
@@ -9,9 +9,32 @@ interface HomeProps extends RouteComponentProps {
 const Home = (props: HomeProps): ReactElement => {
     const start = (): void => props.history.push('Questionnaire');
     return (
-        <div>
-            Home screen
-            <Button text="Start" onClick={start}></Button>
+        <div className="govuk-width-container">
+            <div className="govuk-breadcrumbs">
+                <ol className="govuk-breadcrumbs__list">
+                    <li className="govuk-breadcrumbs__list-item">
+                        <a className="govuk-breadcrumbs__link" href="#">Home</a>
+                    </li>
+                    <li className="govuk-breadcrumbs__list-item">
+                        <a className="govuk-breadcrumbs__link" href="#">Section</a>
+                    </li>
+                    <li className="govuk-breadcrumbs__list-item" aria-current="page">Subsection</li>
+                </ol>
+            </div>
+
+            <main className="govuk-main-wrapper " id="main-content" role="main">
+                <div className="govuk-grid-row">
+                    <div className="govuk-grid-column-two-thirds">
+                        <h1 className="govuk-heading-xl">Loan calculator</h1>
+                        <p className="govuk-body">Use this service to:</p>
+                        <ul className="govuk-list govuk-list--bullet">
+                            <li>Calculate how much a load will cost</li>
+                        </ul>
+                        <p className="govuk-body">Takes less than one minute.</p>
+                        <Button text="Start now" onClick={start}></Button>
+                    </div>
+                </div>
+            </main>
         </div>
     )
 };
