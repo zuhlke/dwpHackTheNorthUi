5cd29a525a27b5744b69696b8c51cfc8f2501a05
Michael Chadwick <michael.chadwick@zuhlke.com>
2020-01-14 11:48:25 +0000

    Home Component now uses MainContent template

--- dwp-hack-the-north-ui/src/Home/Home.tsx
+++ dwp-hack-the-north-ui/src/Home/Home.tsx
@@ -1,32 +1,34 @@
-import React, {ReactElement} from 'react';
-import {RouteComponentProps, withRouter} from 'react-router-dom';
+import React, { ReactElement } from 'react';
+import { RouteComponentProps, withRouter } from 'react-router-dom';
+import { BreadcrumbListProps } from '../common/Breadcrumb/Breadcrumb';
 import Button from "../common/Button/Button";
-import { BreadcrumbList } from '../common/Breadcrumb/Breadcrumb';
+import { MainContent } from '../common/Content/MainContent';
 
 interface HomeProps extends RouteComponentProps {
     id: string;
 }
 
-const Home = (props: HomeProps): ReactElement => {
+const homeContent = (props: HomeProps): ReactElement => {
     const start = (): void => props.history.push('Questionnaire');
     return (
-        <div className="govuk-width-container">
-            <BreadcrumbList parentItems={[]} currentItem={{visibleText: "Home: Loan Calculator"}}/>
+        <div>
+            <h1 className="govuk-heading-xl">Loan calculator</h1>
+            <p className="govuk-body">Use this service to:</p>
+            <ul className="govuk-list govuk-list--bullet">
+                <li>Calculate how much a loan will cost</li>
+                <li>Determine whether you can afford the loan</li>
+            </ul>
+            <p className="govuk-body">Takes less than one minute.</p>
+            <Button text="Start now" onClick={start}></Button>
+        </div>
+    );
+}
 
-            <main className="govuk-main-wrapper " id="main-content" role="main">
-                <div className="govuk-grid-row">
-                    <div className="govuk-grid-column-two-thirds">
-                        <h1 className="govuk-heading-xl">Loan calculator</h1>
-                        <p className="govuk-body">Use this service to:</p>
-                        <ul className="govuk-list govuk-list--bullet">
-                            <li>Calculate how much a loan will cost</li>
-                            <li>Determine whether you can afford the loan</li>
-                        </ul>
-                        <p className="govuk-body">Takes less than one minute.</p>
-                        <Button text="Start now" onClick={start}></Button>
-                    </div>
-                </div>
-            </main>
+const Home = (props: HomeProps): ReactElement => {
+    const breadcrumbProps: BreadcrumbListProps = {parentItems: [], currentItem: {visibleText: "Home: Loan Calculator"}};
+    return (
+        <div>
+            <MainContent breadcrumbData={breadcrumbProps} reactiveContent={homeContent(props)} />
         </div>
     )
 };
