5afc8ce80ec746aeabd40f60ad71ebe0d42bd0c4
Michael Chadwick <michael.chadwick@zuhlke.com>
2020-01-15 10:55:56 +0000

    Loans provide Total Cost and Increment Cost (#3)

--- dwp-hack-the-north-ui/src/Result/Calculator/Loan.test.ts
+++ dwp-hack-the-north-ui/src/Result/Calculator/Loan.test.ts
@@ -1,19 +1,54 @@
-import { Loan, LoanAmount, LoanInterest, LoanTime } from './Loan';
+import { Loan, LoanAmount, LoanCalculator, LoanInterest, LoanTime } from './Loan';
+
+const oneHundredThousand: LoanAmount = LoanAmount.from(100000);
+const oneThousand: LoanAmount = LoanAmount.from(1000);
 
 const fiveYearsInMonths: LoanTime = LoanTime.months(60);
 const fiveYears: LoanTime = LoanTime.years(5);
+const thirtyYears: LoanTime = LoanTime.years(30);
 
-test('Loan time can be obtained in both months and years', async () => {    
+const twelvePercentAnnual: LoanInterest = LoanInterest.annualCompound(0.12);
+const twelvePercentMonthly: LoanInterest = LoanInterest.monthlyCompound(0.01);
+const sixPercentAnnualAsMonthly: LoanInterest = LoanInterest.monthlyCompoundWithAnnualRate(0.06);
+const oneThousandPercentAnnualAsMonthly: LoanInterest = LoanInterest.monthlyCompoundWithAnnualRate(10.00);
+
+const loanOne: LoanCalculator = Loan.of(
+    oneThousand,
+    oneThousandPercentAnnualAsMonthly,
+    fiveYears
+);
+const loanTwo: LoanCalculator = Loan.of(
+    oneHundredThousand,
+    sixPercentAnnualAsMonthly,
+    thirtyYears
+);
+
+test('Loan time can be obtained in both months and years', async () => {
     expect(fiveYearsInMonths.getMonthsTime()).toBe(fiveYears.getMonthsTime());
     expect(fiveYearsInMonths.getYearsTime()).toBe(fiveYears.getYearsTime());
 }, 100);
 
-test('Can return a cost per month based on Loan amount, time and interest rate', async () => {
-    const loanOne: Loan = Loan.of(
-        LoanAmount.from(1000),
-        LoanInterest.from(10.00),
-        fiveYears
-    );
-    
-    expect(loanOne.getMonthlyCost()).toBe(50.00);
+test('Loan Interest Rates can be obtained annually and monthly', async () => {
+    expect(twelvePercentAnnual.getMonthlyRate()).toBe(twelvePercentMonthly.getMonthlyRate());
+    expect(twelvePercentAnnual.getAnnualRate()).toBe(twelvePercentMonthly.getAnnualRate());
+}, 100);
+
+test('Can return a total cost based on Loan amount, time and interest rate', async () => {
+    expect(loanOne.getTotalPaymentCost()).toBe(6229971881013844992.00);
+}, 100);
+
+test('Loans have a total number of periodic payments, based on the number of payments a year and the number of years', async () => {
+    expect(loanTwo.getPeriodicPayments()).toBe(360);
+}, 100);
+
+test('Loan Rates can provide the periodic interest rate', async () => {
+    expect(sixPercentAnnualAsMonthly.getPeriodicInterestRate()).toBe(0.005);
+}, 100);
+
+test('Loans have a discount factor, taking into consideration the periodic payments and periodic interest rate', async () => {
+    expect(loanTwo.getDiscountFactorWithPrecisionLevel(4)).toBe(166.7916);
+}, 100);
+
+test('Loans can return a cost per increment based on Loan amount, time and interest rate', async () => {
+    expect(loanTwo.getIncrementPaymentCost()).toBe(599.55);
 }, 100);
--- dwp-hack-the-north-ui/src/Result/Calculator/Loan.ts
+++ dwp-hack-the-north-ui/src/Result/Calculator/Loan.ts
@@ -9,19 +9,56 @@ export class LoanAmount {
     public static from(value: number): LoanAmount {
         return new LoanAmount(value);
     }
+
+    public getTotal(): number {
+        return this.value;
+    }
 }
+
 
 export class LoanInterest {
-    private readonly value: number;
+    private readonly annualPercentageRate: number;
+    private readonly compoundIncrementCount: number;
 
-    private constructor(value: number) {
-        this.value = value;
+    private constructor(value: number, compoundIncrements: number) {
+        this.annualPercentageRate = Number(value.toFixed(2));
+        this.compoundIncrementCount = compoundIncrements;
     }
 
-    public static from(value: number): LoanInterest {
-        return new LoanInterest(value);
+    public static of(annualInterestRate: number, compoundIncrementCount: number): LoanInterest {
+        return new LoanInterest(annualInterestRate, compoundIncrementCount);
     }
+
+    public static annualCompound(annualPercentageRate: number): LoanInterest {
+        return this.of(annualPercentageRate, 1);
+    }
+
+    public static monthlyCompound(monthlyPercentageRate: number): LoanInterest {
+        return this.of(monthlyPercentageRate * 12, 12);
+    }
+
+    public static monthlyCompoundWithAnnualRate(annualPercentageRate: number): LoanInterest {
+        return this.of(annualPercentageRate, 12);
+    }
+
+    public getMonthlyRate(): number {
+        return Number((this.annualPercentageRate / 12).toFixed(2));
+    }
+
+    public getAnnualRate(): number {
+        return this.annualPercentageRate;
+    }
+
+    public getCompoundIncrementCount(): number {
+        return this.compoundIncrementCount;
+    }
+
+    public getPeriodicInterestRate(): number {
+        return this.annualPercentageRate / this.compoundIncrementCount;
+    }
+
 }
+
 
 export class LoanTime {
     private readonly years: number;
@@ -47,7 +84,15 @@ export class LoanTime {
     }
 }
 
-export class Loan {
+
+export interface LoanCalculator {
+    getDiscountFactorWithPrecisionLevel(precisionLevel: number): number;
+    getIncrementPaymentCost(): number;
+    getPeriodicPayments(): number;
+    getTotalPaymentCost(): number;
+}
+
+export class Loan implements LoanCalculator {
     private readonly amount: LoanAmount;
     private readonly length: LoanTime;
     private readonly rate: LoanInterest;
@@ -62,7 +107,30 @@ export class Loan {
         return new Loan(amount, rate, length);
     }
 
-    public getMonthlyCost(): number {
-        return 50.00;
+    // D = {[(1 + r) ^n] - 1} / [r(1 + r)^n]}
+    private getDiscountFactor(): number {
+        const commonBracket = Math.pow((1 + this.rate.getPeriodicInterestRate()), this.getPeriodicPayments());
+        return (commonBracket - 1) / (this.rate.getPeriodicInterestRate() * commonBracket);
+    }
+
+    public getDiscountFactorWithPrecisionLevel(precisionLevel: number): number {
+        return Number(this.getDiscountFactor().toFixed(precisionLevel));
+    }
+
+    // A / D
+    public getIncrementPaymentCost(): number {
+        return Number((this.amount.getTotal() / this.getDiscountFactor()).toFixed(2));
+    }
+
+    public getPeriodicPayments(): number {
+        return this.rate.getCompoundIncrementCount() * this.length.getYearsTime();
+    }
+
+    // (1 + r/n)^(nt)
+    public getTotalPaymentCost(): number {
+        const initialBracket: number = 1 + this.rate.getPeriodicInterestRate();
+        const power: number = this.getPeriodicPayments();
+
+        return this.amount.getTotal() * Number(Math.pow(initialBracket, power).toFixed(2));
     }
 }
