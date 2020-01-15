94aca34e51fa42f786ac569d06336f700a35a9bc
ajum <chemistric-05@mail.ru>
2020-01-15 13:27:58 +0000

    #6 Refactor deployment bucket into a construct

--- provisioning/lib/provisioning-stack.ts
+++ provisioning/lib/provisioning-stack.ts
@@ -1,22 +1,10 @@
 import * as cdk from '@aws-cdk/core';
-import s3 = require("@aws-cdk/aws-s3");
-import s3deploy = require("@aws-cdk/aws-s3-deployment");
+import {HostingBucket} from "./hosting-bucket";
 
 export class ProvisioningStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const deploymentBucket = new s3.Bucket(this, 'dwp-hack-the-north-bucket', {
-      bucketName: 'z10020-dwp-hack-the-north',
-      websiteIndexDocument: 'index.html',
-      removalPolicy: cdk.RemovalPolicy.DESTROY, // the bucket must be empty before running `cdk destroy`
-    });
-
-    new s3deploy.BucketDeployment(this, 'deploy-wesbite', {
-      sources: [s3deploy.Source.asset('../dwp-hack-the-north-ui/build')],
-      destinationBucket: deploymentBucket
-    });
-
-    deploymentBucket.grantPublicAccess()
+    new HostingBucket(this, 'hosting-bucket')
   }
 }
