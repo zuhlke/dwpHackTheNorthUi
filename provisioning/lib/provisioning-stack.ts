807f076c7b68494ec2428fef27cdeb2e8d59f949
ajum <chemistric-05@mail.ru>
2020-01-15 09:59:45 +0000

    #6 Deploy the frontend to S3 bucket.

--- provisioning/lib/provisioning-stack.ts
+++ provisioning/lib/provisioning-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from '@aws-cdk/core';
 import s3 = require("@aws-cdk/aws-s3");
+import s3deploy = require("@aws-cdk/aws-s3-deployment");
 
 export class ProvisioningStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -11,6 +12,10 @@ export class ProvisioningStack extends cdk.Stack {
       removalPolicy: cdk.RemovalPolicy.DESTROY, // the bucket must be empty before running `cdk destroy`
     });
 
+    new s3deploy.BucketDeployment(this, 'deploy-wesbite', {
+      sources: [s3deploy.Source.asset('../dwp-hack-the-north-ui/build')],
+      destinationBucket: deploymentBucket
+    });
 
     deploymentBucket.grantPublicAccess()
   }
