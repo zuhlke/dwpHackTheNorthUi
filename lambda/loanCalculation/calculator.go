0ec78e4f5c6aea6b8b9f27e9b02c98c1e5faf5bc
Michael Chadwick <michael.chadwick@zuhlke.com>
2020-01-31 14:36:56 +0000

    Completed Loan in Golang backend (#11)
    - Implement monthly cost within Loan struct
    - Implement total cost within Loan struct
    - Implement Discount Factor within Loan struct

--- lambda/loanCalculation/calculator.go
+++ lambda/loanCalculation/calculator.go
@@ -1,5 +1,9 @@
 package calculator
 
+import (
+	"math"
+)
+
 // LoanTime describes the duration of the loan.
 type LoanTime struct {
 	months int
@@ -8,6 +12,18 @@ type LoanTime struct {
 // LoanInterest describes the APR of the loan.
 type LoanInterest struct {
 	annualCompound float64
+}
+
+// LoanAmount contains the initial amount borrowed.
+type LoanAmount struct {
+	amount int
+}
+
+// Loan contains the aggregated details of the loan.
+type Loan struct {
+	loanInterest LoanInterest
+	loanTime LoanTime
+	loanAmount LoanAmount
 }
 
 // GetYearsTime returns the number of years that the struct holds, as the
@@ -19,4 +35,30 @@ func (lt LoanTime) GetYearsTime() float64 {
 // GetMonthlyCompound returns the monthly interest derived from the annual one.
 func (li LoanInterest) GetMonthlyCompound() float64 {
 	return li.annualCompound / 12
+}
+
+// GetDiscountFactor is the magic intermediary number which can provide the monthly payment cost when you divide the total loan amount by it.
+func (l Loan) GetDiscountFactor() float64 {
+	var commonBracket = math.Pow((1 + l.loanInterest.GetMonthlyCompound()), float64(l.loanTime.months))
+	return (commonBracket - 1) / (l.loanInterest.GetMonthlyCompound() * commonBracket)
+}
+
+func round(value float64, precision int) float64 {
+	var precisionDecimal = math.Pow(10.0, float64(precision))
+	return math.Trunc(value * precisionDecimal + 0.5) / precisionDecimal
+}
+
+// GetDiscountFactorWithPrecisionLevel is the same as GetDiscountFactor but to a given precision level.
+func (l Loan) GetDiscountFactorWithPrecisionLevel(precision int) float64 {
+	return round(l.GetDiscountFactor(), precision)
+}
+
+// GetMonthlyPayment calculates the monthly payment
+func (l Loan) GetMonthlyPayment() float64 {
+	return round(float64(l.loanAmount.amount) / l.GetDiscountFactor(), 2)
+}
+
+// GetTotalCost calculates the total repayed
+func (l Loan) GetTotalCost() float64 {
+	return round(l.GetMonthlyPayment() * float64(l.loanTime.months), 2)
 }
--- lambda/loanCalculation/calculator_test.go
+++ lambda/loanCalculation/calculator_test.go
@@ -10,18 +10,29 @@ var fiveYears = LoanTime{months: 60}
 var oneAndAHalfYears = LoanTime{months: 18}
 var twelvePercentAnnual = LoanInterest{annualCompound: 0.12}
 
+var loanOne = Loan{loanTime: LoanTime{months: 360}, loanInterest: LoanInterest{annualCompound: 0.06}, loanAmount: LoanAmount{amount: 100000}}
+
 func TestGetYears(t *testing.T) {
-	t.Log("Loan time can be obtained in both months and years")
-	Equal(t, fiveYears.GetYearsTime(), 5.0)
+	t.Log("Loan time can handle returning decimal places for Years")
+	Equal(t, 1.5, oneAndAHalfYears.GetYearsTime())
 }
 
-func TestGetYearsNonIntegral(t *testing.T) {
-	t.Log("Loan time can be obtained in both months and years")
-	Equal(t, oneAndAHalfYears.GetYearsTime(), 1.5)
+func TestMonthlyInterestRate(t *testing.T) {
+	t.Log("Loan Interest Rates can derive the Monthly compound interest rate from the Annual rate")
+	Equal(t, 0.01, twelvePercentAnnual.GetMonthlyCompound())
 }
 
-func TestAnnualInterestRate(t *testing.T) {
-	t.Log("Loan Interest Rates can be obtained annually")
-	Equal(t, twelvePercentAnnual.GetMonthlyCompound(), 0.01)
+func TestLoanDiscountFactor(t *testing.T) {
+	t.Log("Loans have a discount factor, taking into consideration the periodic payments and periodic interest rate")
+	Equal(t, 166.7916, loanOne.GetDiscountFactorWithPrecisionLevel(4))
+}
 
+func TestMonthlyPayment(t *testing.T) {
+	t.Log("Loans can return a cost per increment based on Loan amount, time and interest rate")
+	Equal(t, 599.55, loanOne.GetMonthlyPayment())
+}
+
+func TestTotalCost(t *testing.T) {
+	t.Log("Can return a total cost based on Loan amount, time and interest rate")
+	Equal(t, 215838.0, loanOne.GetTotalCost())
 }
