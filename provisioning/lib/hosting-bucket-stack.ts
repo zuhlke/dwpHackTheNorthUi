dca77072495fa6cf170bdaa6abdd8b063503cb48
ajum <chemistric-05@mail.ru>
2020-02-05 12:11:01 +0000

    #16 (Refactor): replace require statement when importing S3 package

--- provisioning/lib/hosting-bucket-stack.ts
+++ provisioning/lib/hosting-bucket-stack.ts
@@ -1,6 +1,6 @@
-import s3 = require("@aws-cdk/aws-s3");
 import {App, RemovalPolicy, Stack} from "@aws-cdk/core";
 import {BucketDeployment, Source} from "@aws-cdk/aws-s3-deployment";
+import {Bucket} from "@aws-cdk/aws-s3";
 
 export interface HostingBucketProps {
     bucketName: string;
@@ -12,7 +12,7 @@ export class HostingBucketStack extends Stack {
         super(scope, id);
 
         if ((props.bucketName !== null && props.bucketName !== "") && (props.sourcePath !== null && props.sourcePath !== "")) {
-            const deploymentBucket = new s3.Bucket(this, 'deployment-bucket', {
+            const deploymentBucket = new Bucket(this, 'deployment-bucket', {
                 bucketName: props.bucketName,
                 websiteIndexDocument: 'index.html',
                 removalPolicy: RemovalPolicy.DESTROY, // bucket must be empty before performing 'cdk destroy'
