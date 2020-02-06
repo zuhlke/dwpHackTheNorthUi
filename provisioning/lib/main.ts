8b2159e6f1d4e3ebcfe683fa738df37bf5bbe3ef
ajum <chemistric-05@mail.ru>
2020-02-06 14:17:22 +0000

    #15 #20 Add an array with Lambdas and deploy API gateway with multiple resources

--- provisioning/lib/main.ts
+++ provisioning/lib/main.ts
@@ -1,26 +1,41 @@
 #!/usr/bin/env node
 import {App, Tag} from "@aws-cdk/core";
 import {HostingBucketStack} from "./hosting-bucket-stack";
-import {LambdaAndApiGatewayStack} from "./lambda-and-api-gateway-stack";
+import {LambdaAndApiGatewayStack, LambdaStackProps} from "./lambda-and-api-gateway-stack";
 
 const hostingBucketName = 'z10020-dwp-hack-the-north';
 const frontendSourcePath = '../dwp-hack-the-north-ui/build';
-const getQuestionsHandler = 'main';
-const getQuestionsSource = '../../lambda/questionnaire';
-const uri = '';
-const dbName = '';
-const collection = '';
+
+const lambdas: LambdaStackProps[] = [
+    {
+        functionName: 'getQuestions',
+        resourceName: 'questions',
+        srcDir: '../../lambda/questionnaire',
+        handler: 'main',
+        uri: '',
+        dbName: '',
+        collection: ''
+    },
+    {
+        functionName: 'getLoanLambda',
+        resourceName: 'loan',
+        srcDir: '../../lambda/calculator',
+        handler: "main"
+    }];
 
 export class Main extends App {
     constructor() {
         super();
-        new HostingBucketStack(this, 'HostingBucketStack', {bucketName: hostingBucketName, sourcePath: frontendSourcePath});
-        new LambdaAndApiGatewayStack(this, 'LambdaAndApiGatewayStack', {srcDir: getQuestionsSource, handler: getQuestionsHandler, uri: uri, dbName: dbName, collection: collection});
+        new HostingBucketStack(this, 'HostingBucketStack', {
+            bucketName: hostingBucketName,
+            sourcePath: frontendSourcePath
+        });
+        new LambdaAndApiGatewayStack(this, 'LambdaAndApiGatewayStack', lambdas);
     }
 }
 
 const mainConstruct = new Main();
-Tag.add(mainConstruct,'ProjectName', 'DWP-Hack-the-North');
+Tag.add(mainConstruct, 'ProjectName', 'DWP-Hack-the-North');
 Tag.add(mainConstruct, 'ProjectOwner', 'Manchester');
 Tag.add(mainConstruct, 'ProjectCode', 'z10020');
 
