35d2c18b7507927d8f75103592a99ed255f092f7
ajum <chemistric-05@mail.ru>
2020-02-06 14:15:05 +0000

    #15 Add response model to GET and POST methods. Refactor the Lambda deployment and API Gateway creation into functions.

--- provisioning/lib/lambda-and-api-gateway-stack.ts
+++ provisioning/lib/lambda-and-api-gateway-stack.ts
@@ -2,68 +2,108 @@ import lambda = require("@aws-cdk/aws-lambda");
 import path = require("path");
 import apigateway = require("@aws-cdk/aws-apigateway");
 import {App, Construct, Stack} from "@aws-cdk/core";
+import {EmptyModel} from "@aws-cdk/aws-apigateway";
 
 export interface LambdaStackProps {
+    functionName: string;
+    resourceName: string;
     srcDir: string;
     handler: string;
-    uri: string;
-    dbName: string;
-    collection: string;
+    uri?: string;
+    dbName?: string;
+    collection?: string;
 }
 
 class LambdaAndApiGatewayConstruct extends Construct {
-    constructor(scope: Construct, id: string, props: LambdaStackProps) {
+    constructor(scope: Construct, id: string, lambdas: LambdaStackProps[]) {
         super(scope, id);
+        let api = undefined;
 
-        const handler = new lambda.Function(this, 'GetQuestions', {
-            functionName: "getQuestions",
+        for (let i = 0; i < lambdas.length; i++) {
+            const handler = this.createLambdaFunction(lambdas[i]);
+            this.addDatabaseEnvironmentVariables(lambdas[i], handler);
+            const integration = this.createLambdaIntegration(handler);
+            if (api === undefined) {
+                api = this.createApiGateway(handler);
+            }
+            const resource = this.addResource(api, lambdas[i].resourceName);
+            this.addHttpGetMethod(resource, integration);
+            this.addHttpPostMethod(resource, integration);
+            this.enableCors(resource, handler);
+
+        }
+    }
+
+    createLambdaFunction(props: LambdaStackProps): lambda.Function {
+        return new lambda.Function(this, props.functionName, {
+            functionName: props.functionName,
             runtime: lambda.Runtime.GO_1_X,
             handler: props.handler,
             code: lambda.Code.fromAsset(path.join(__dirname, props.srcDir)),
-            environment: {
-                URI: props.uri,
-                DATABASE_NAME: props.dbName,
-                COLLECTION: props.collection
-            }
         });
 
-        const api = new apigateway.LambdaRestApi(this, 'api-gateway', {
+    }
+
+    addDatabaseEnvironmentVariables(props: LambdaStackProps, handler: lambda.Function): void {
+        if (props.uri && props.dbName && props.collection) {
+            handler.addEnvironment("URI", props.uri!);
+            handler.addEnvironment("DATABASE_NAME", props.dbName!);
+            handler.addEnvironment("COLLECTION", props.collection!);
+        }
+    }
+
+    createApiGateway(handler: lambda.Function): apigateway.LambdaRestApi {
+        return new apigateway.LambdaRestApi(this, 'api-gateway', {
             restApiName: "API Service",
             description: "This service serves DWP Hack the North",
             handler: handler,
             proxy: false
         });
+    }
 
-        const getQuestionsIntegration = new apigateway.LambdaIntegration(handler, {
+    createLambdaIntegration(handler: lambda.Function): apigateway.LambdaIntegration {
+        return new apigateway.LambdaIntegration(handler, {
             proxy: false,
-            requestTemplates: {"application/json": '{ "statusCode": "200" }'},
             integrationResponses: [{
                 statusCode: '200',
             }],
         });
+    }
 
-        const questions = api.root.addResource('questions');
-        questions.addMethod('GET', getQuestionsIntegration, {
-            methodResponses: [{
-             statusCode: '200',
-                responseParameters: {
-                    'method.response.header.Access-Control-Allow-Methods': true
-                },
-                responseModels: {
-                }
-            }],
-        });
+    addResource(api: apigateway.LambdaRestApi, resourceName: string): apigateway.Resource {
+        return api.root.addResource(resourceName);
+    }
 
-        questions.addMethod('POST', getQuestionsIntegration, {
+    addHttpGetMethod(resource: apigateway.Resource, integration: apigateway.LambdaIntegration): void {
+        resource.addMethod('GET', integration, {
             methodResponses: [{
                 statusCode: '200',
                 responseParameters: {
                     'method.response.header.Access-Control-Allow-Methods': true
                 },
+                responseModels: {
+                    'application/json': new EmptyModel()
+                }
+            }],
+        });
+    }
+
+    addHttpPostMethod(resource: apigateway.Resource, integration: apigateway.LambdaIntegration): void {
+        resource.addMethod('POST', integration, {
+            methodResponses: [{
+                statusCode: '200',
+                responseParameters: {
+                    'method.response.header.Access-Control-Allow-Methods': true
+                },
+                responseModels: {
+                    'application/json': new EmptyModel()
+                }
             }]
         });
+    }
 
-        questions.addMethod('OPTIONS', new apigateway.LambdaIntegration( handler,{
+    enableCors(resource: apigateway.Resource, handler: lambda.Function): void {
+        resource.addMethod('OPTIONS', new apigateway.LambdaIntegration(handler, {
             integrationResponses: [{
                 statusCode: '200',
                 responseParameters: {
@@ -92,7 +132,7 @@ class LambdaAndApiGatewayConstruct extends Construct {
 }
 
 export class LambdaAndApiGatewayStack extends Stack {
-    constructor(app: App, id: string, props: LambdaStackProps) {
+    constructor(app: App, id: string, props: LambdaStackProps[]) {
         super(app, id);
 
         new LambdaAndApiGatewayConstruct(this, id, props);
